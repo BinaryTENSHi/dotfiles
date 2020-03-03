6bf79ec3336dfcd74c13334265382d972b873c11
BinaryTENSHi <binarytenshi@gmail.com>
2020-03-03 16:10:01 +0100

    Initial doom config

---
+++ doom/config.el
@@ -0,0 +1,7 @@
+;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
+
+;; Font
+(setq doom-font (font-spec :family "JetBrainsMono" :size 10.0))
+
+;; Set default work path
+(setq projectile-project-search-path '("~/work"))
