24d4763ed50cc2a6d68cb02baafbdb2eb13fcf79
BinaryTENSHi <binarytenshi@gmail.com>
2020-03-03 17:55:17 +0100

    Emacs client configuration

--- doom/config.el
+++ doom/config.el
@@ -1,7 +1,12 @@
 ;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
 
 ;; Font
-(setq doom-font (font-spec :family "JetBrainsMono" :size 10.0))
+;; Specifying JetBrains Mono as the font messes up the bottom part of an emacsclient. Why? I don't know.
+;;(setq doom-font (font-spec :family "JetBrainsMono" :size 10.0))
 
 ;; Set default work path
 (setq projectile-project-search-path '("~/work"))
+
+;; Do not create an anonymous session #n when launching emacsclient
+(after! persp-mode
+  (setq persp-emacsclient-init-frame-behaviour-override "main"))
